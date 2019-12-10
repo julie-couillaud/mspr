a7513b9ed71439e217f57df0fbe4fa9ab915356f
Julie <julie.couillaud@ecoles-wis.net>
2019-12-10 12:01:49 +0100

    update_avantmanger

--- recipies.php
+++ recipies.php
@@ -1,5 +1,38 @@
 <?php require_once 'includes/header.php'; ?>
 
+<?php
+
+//recupère le paramètre d'URL nommé id
+$id = $_GET['id'];
+
+// récupérer les albums
+$sql = "SELECT *
+			FROM recipies
+			WHERE id = :id";
+
+//envoie notre requête au serveur MySQL, sans l'exécuter
+$stmt = $connexion -> prepare($sql);
+//exécute la requête
+$stmt -> execute([":id"=> $id]);
+//nous retourne les résultats
+$albums = $stmt -> fetchAll();
+
+
+// récupérer les critiques
+$sql = "SELECT *
+			FROM comments
+			WHERE user_id = :id
+			ORDER BY date_created DESC";
+
+//envoie notre requête au serveur MySQL, sans l'exécuter
+$stmt = $connexion -> prepare($sql);
+//exécute la requête
+$stmt -> execute([":id" => $id]);
+//nous retourne les résultats
+$comments = $stmt -> fetchAll();
+
+?>
+
 <h1> PAGE RECETTE </h1>
     <div class="card">
         <div class="card-header">
@@ -12,17 +45,17 @@
         </div>
     </div>
 
-    <h3>Dernières critiques</h3>
+    <h2>Dernières critiques</h2>
 
     <div class="last_critiques">
 
         <?php
         foreach ($comments as $comment){
 
-            echo '<h5>' . $comment ['recipie'] . '</h5>';
+            echo '<h5>' . $comment ['recipies'] . '</h5>';
 
             // compose une "barre" comprenant la note, le pseudo et la date de création de l'article
-            echo '<div>' . "Par " . $comment ['pseudo'] . ", le " . $comment ['date_created'] . '</div>';
+            echo '<div>' . "Par " . $comment ['user_id'] . ", le " . $comment ['date_created'] . '</div>';
             echo '</div>';
 
             echo '<div>' . $comment ['content'] . '</div>';
